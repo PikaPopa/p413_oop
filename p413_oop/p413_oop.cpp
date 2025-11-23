b799ae8eb3827b3b69679a7a7a94e1a7da23799b
WorkingJoe <cemka003@gmail.com>
2025-11-23 15:22:24 +0300

    Task done

--- p413_oop/p413_oop.cpp
+++ p413_oop/p413_oop.cpp
@@ -1,6 +1,38 @@
 ﻿#include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Employer {
+public:
+    virtual void print() = 0;
+};
+
+class President : public Employer {
+public:
+    void print() override {
+        std::cout << "President: Manages the entire organization." << std::endl;
+    }
+};
+
+class Manager : public Employer {
+public:
+    void print() override {
+        std::cout << "Manager: Manages departments and teams." << std::endl;
+    }
+};
+
+class Worker : public Employer {
+public:
+    void print() override {
+        std::cout << "Worker: Performs specific tasks and operations." << std::endl;
+    }
+};
+
+int main() {
+    President putin;
+    Manager scarlett;
+    Worker joe;
+
+    putin.print();
+    scarlett.print();
+    joe.print();
 }
