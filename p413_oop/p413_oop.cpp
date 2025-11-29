5a78918f017fe60f74698b0ae0f042af23da8991
WorkingJoe <cemka003@gmail.com>
2025-11-29 14:38:51 +0300

    Task done

--- p413_oop/p413_oop.cpp
+++ p413_oop/p413_oop.cpp
@@ -1,6 +1,89 @@
 ﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Stack {
+private:
+    vector<int> stack;
+public:
+    Stack() {}
+
+    Stack(vector<int> s) : stack(s) {}
+
+    void push(int value) {
+        if (isFull() == 0) {
+            stack.push_back(value);
+        }
+        else {
+            cout << "Stack is full!";
+        }
+    }
+
+    int pop() {
+        if (isEmpty() == 0) {
+            int value = stack.back();
+            stack.pop_back();
+            return value;
+        }
+        else {
+            cout << "Stack is empty!";
+            return -1;
+        }
+    }
+
+    int top() {
+        if (isEmpty() == 0) {
+            return stack.back();
+        }
+        else {
+            cout << "Stack is empty!";
+            return -1;
+        }
+    }
+
+    bool isEmpty() {
+        return stack.empty();
+    }
+
+    bool isFull() {
+        if (stack.capacity() == size()) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    int size() {
+        return stack.max_size();
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Stack s;
+
+    if (s.isEmpty()) {
+        cout << "Stack is empty." << endl;
+    }
+    else {
+        cout << "Stack is not empty." << endl;
+    }
+
+    s.push(5);
+    s.push(10);
+    s.push(15);
+
+    cout << "Top element: " << s.top() << endl;
+
+    int removed = s.pop();
+    cout << "Removed: " << removed << endl;
+
+    cout << "Top element: " << s.top() << endl;
+
+    s.pop();
+    s.pop();
+
+    s.pop();
 }
